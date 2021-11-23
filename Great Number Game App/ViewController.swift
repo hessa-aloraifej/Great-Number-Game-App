15f06e4a563ff1bbb6c47c962ba447e57a62f960
Hessa Aloraifej <aloraifej.hessa@gmail.com>
2021-11-23 12:44:30 +0300

    Initial Commit

---
+++ Great Number Game App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Great Number Game App
+//
+//  Created by Hessa Aloraifej on 23/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Great Number Game AppTests/Great_Number_Game_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Great_Number_Game_AppTests.swift
+//  Great Number Game AppTests
+//
+//  Created by Hessa Aloraifej on 23/11/2021.
+//
+
+import XCTest
+@testable import Great_Number_Game_App
+
+class Great_Number_Game_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
